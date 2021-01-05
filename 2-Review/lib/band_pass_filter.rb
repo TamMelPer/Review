c4ffaac96d77dc75c02f1f2488bdf43374a94e4a
Melissa <mystic101@gmail.com>
2021-01-05 09:43:17 +0000

    returns a filtered soundwave if a frequency is lower than the min frequency

--- 2-Review/lib/band_pass_filter.rb
+++ 2-Review/lib/band_pass_filter.rb
@@ -2,9 +2,17 @@ class BandPassFilter
 
   def initialize(soundwave, min_frequency, max_frequency)
     @soundwave = soundwave
+    @min_frequency = min_frequency
   end
 
   def filter
+    @soundwave.map! do |frequency|
+      if frequency < @min_frequency
+        frequency = @min_frequency
+      else
+        frequency
+      end
+    end
     @soundwave
   end
 
--- 2-Review/spec/band_pass_filter_spec.rb
+++ 2-Review/spec/band_pass_filter_spec.rb
@@ -10,4 +10,12 @@ describe '#filter' do
     beatles = BandPassFilter.new([90,100], 90, 110)
     expect(beatles.filter).to eq [90,100]
   end
+
+  it 'returns a filtered soundwave if a frequency is lower than the min frequency' do
+    beatles = BandPassFilter.new([80, 90, 100, 110, 120], 90, 120)
+    expect(beatles.filter).to eq [90, 90, 100, 110, 120]
+  end
+
+
+
 end
