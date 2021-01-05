a4663d21bbb8345007702ef831953b40276a99e3
Melissa <mystic101@gmail.com>
2021-01-05 10:29:16 +0000

    Renamed folder

---
+++ 2-Band Pass Filter/lib/band_pass_filter.rb
@@ -0,0 +1,24 @@
+class BandPassFilter
+
+
+  def initialize(soundwave, min_frequency = 40, max_frequency = 1000)
+    @soundwave = soundwave
+    @min_frequency = min_frequency
+    @max_frequency = max_frequency
+
+  end
+
+  def filter
+    @soundwave.map! do |frequency|
+      if frequency < @min_frequency
+        frequency = @min_frequency
+      elsif frequency > @max_frequency
+        frequency = @max_frequency
+      else
+        frequency
+      end
+    end
+    @soundwave
+  end
+
+end
